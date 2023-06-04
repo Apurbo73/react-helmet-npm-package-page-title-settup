67ffb9a4b44b470a7fe8b567694fff74ed5dd582
Apurbo73 <apurbodebnath895@gmail.com>
2023-06-04 16:26:21 +0600

    simple

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Helmet } from 'react-helmet'
+function App() {
+
+  return (
+  <div>
+  <Helmet>
+    <title>Home Page</title>
+  </Helmet>
+    hi
+  </div>
+  )
+}
+
+export default App
